2908b50fcff3911fe5995fc78c020236a6fef2c1
Dimitar Todorov <d.todorov.vn@gmail.com>
2018-10-12 12:29:40 +0300

    Lesson-10 - Finish tuning of register user layouts

--- app/src/main/java/com/example/dtodorov/smack/Utilities/Constants.kt
+++ app/src/main/java/com/example/dtodorov/smack/Utilities/Constants.kt
@@ -4,3 +4,6 @@ const val BASE_URL = "https://multi-chat-chat.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER = "${BASE_URL}user/add"
+
+//Broad cast constants
+const val BROADCAST_USER_DATA_CHANGED = "BROADCAST_USER_DATE_CHANGED"
